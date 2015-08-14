1792ae9c55bea0b9e1d009ff1a2436878db6d305
Jean-Alexandre Peyroux <m@ja.pe>
2015-08-14 16:46:29 +0200

    modif du setup.py

--- apiwatcherlink/run.py
+++ apiwatcherlink/run.py
@@ -3,26 +3,30 @@ from apiwatcherlink.rest.page import PageView
 from apiwatcherlink.rest.snap import SnapView
 from apiwatcherlink.rest.diff import DiffView
 
-#
-# Page
-#
-pageview = PageView.as_view('pageview')
-app.add_url_rule('/page/<id>/', view_func=pageview, methods=['GET'])
-app.add_url_rule('/page/', view_func=pageview, methods=['POST'])
+def start():
+    #
+    # Page
+    #
+    pageview = PageView.as_view('pageview')
+    app.add_url_rule('/page/<id>/', view_func=pageview, methods=['GET'])
+    app.add_url_rule('/page/', view_func=pageview, methods=['POST'])
 
-#
-# Snap
-#
-snapview = SnapView.as_view('snapview')
-# GET : get du snap
-app.add_url_rule('/snap/<id>/', view_func=snapview, methods=['GET'])
-# POST : creat snap de la pageid | GET : return snaps de la page
-app.add_url_rule('/snap/', view_func=snapview, methods=['POST'])
+    #
+    # Snap
+    #
+    snapview = SnapView.as_view('snapview')
+    # GET : get du snap
+    app.add_url_rule('/snap/<id>/', view_func=snapview, methods=['GET'])
+    # POST : creat snap de la pageid | GET : return snaps de la page
+    app.add_url_rule('/snap/', view_func=snapview, methods=['POST'])
 
-#
-# Diff
-#
-diffview = DiffView.as_view('diffview')
-app.add_url_rule('/diff/<snapa>/<snapb>/', view_func=diffview, methods=['GET'])
+    #
+    # Diff
+    #
+    diffview = DiffView.as_view('diffview')
+    app.add_url_rule('/diff/<snapa>/<snapb>/', view_func=diffview, methods=['GET'])
 
-app.run("0.0.0.0", port=5050)
+    app.run("0.0.0.0", port=5050)
+
+if __name__ == "__main__":
+    start()
--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ from distutils.core import setup
 setup(
     name='api.watcher.link',
     version='0.1',
-    packages=[''],
+    packages=['apiwatcherlink', 'apiwatcherlink.model', 'apiwatcherlink.rest', 'apiwatcherlink.utils'],
     url='http://api.watcher.link',
     license='',
     author='ja',
     author_email='m@ja.pe',
-    description='', requires=['flask',
-                              'requests',
-                              'mongoengine',
-                              'beautifulsoup4',
-                              'selenium']
+    scripts=['scripts/apiwatcherlink'],
+    description='',
+    requires=['flask',
+              'requests',
+              'mongoengine',
+              'beautifulsoup4',
+              'selenium']
 )
